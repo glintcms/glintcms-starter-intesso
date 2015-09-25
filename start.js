98d5d1a322b4e4e9174fccb0ce82c6ec99c16af4
andi-neck <andi.neck@intesso.com>
2015-09-25 14:00:54 +0200

    test postversion

--- start.js
+++ start.js
@@ -4,7 +4,7 @@ module.exports = function start(args) {
   var port = args.port || process.env.PORT || 8080;
   app.listen(port);
   console.log("server pid %s listening on port %s in %s mode", process.pid, port, process.env.NODE_ENV);
-}
+};
 
 if (require.main === module) {
   var args = require('subarg')(process.argv.slice(2));
